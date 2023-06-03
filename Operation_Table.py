4ed560ef60c685b703d6ccb76980b278512acfde
MarynaLysiak <123303979+MarynaLysiak@users.noreply.github.com>
2023-06-03 12:27:17 +0300

    Update Operation_Table.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- Operation_Table.py
+++ Operation_Table.py
@@ -24,6 +24,6 @@ def print_operation_table(operation, num_rows = 6, num_columns = 6):
         product = [] # новый список для произведений индексов row и column
         for j in range(1, num_columns + 1):
             product.append(str(operation(i, j)))
-        print(" ".join(product))
+        print("\t".join(product))
 
 print_operation_table(lambda x, y: x * y)
